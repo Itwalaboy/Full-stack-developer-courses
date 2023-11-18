0c19b52a8e3ca9517a736a17dc83242e5fbfa86f
Itwalaboy <150066142+Itwalaboy@users.noreply.github.com>
2023-11-18 12:12:07 +0530

    Add files via upload

--- check.js
+++ check.js
@@ -1,5 +1,7 @@
-var items =document.getElementsbyClassName('list-group-items');
-console.log(items);
-console.log(items[1]);
-items[1].textContent='bold';
-items[1].style.backgroundcolor='green';
+console.log("title");
+title[0].textContent="Hello";
+var odd = document.querySelectorAll('li:nth-child(odd)');
+var even = document.querySelectorAll('li:nth-child(even)')
+for(var i=0; i<odd.length; i++){
+	odd[i].style.backgroundcolor="green";
+	even[i].style.backgroundcolor="green";}
